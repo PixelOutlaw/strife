4c573ad89916b7a54a8a11487f4853029fb93c44
Richard Harrah <topplethenunnery@gmail.com>
2014-10-17 19:48:55 -0400

    removing useless field

--- src/main/java/info/faceland/strife/attributes/StrifeAttribute.java
+++ src/main/java/info/faceland/strife/attributes/StrifeAttribute.java
@@ -9,41 +9,38 @@
 package info.faceland.strife.attributes;
 
 import org.bukkit.ChatColor;
-import org.bukkit.Sound;
 
 public enum StrifeAttribute {
 
     // modify any attributes here
-    MELEE_DAMAGE("Melee Damage", ChatColor.YELLOW, "Maximum damage dealt by melee attacks", 1, false, null),
-    RANGED_DAMAGE("Ranged Damage", ChatColor.YELLOW, "Maximum damage dealt by bow", 1, false, null),
-    CRITICAL_RATE("Critical Rate", ChatColor.YELLOW, "Chance of landing a critical strike", 0, true, null),
-    CRITICAL_DAMAGE("Critical Damage", ChatColor.YELLOW, "100% Critical Damage = no bonus. 200% Critical Damage = 2x.", 1.5D, true, null),
-    ATTACK_SPEED("Attack Speed", ChatColor.YELLOW, "100% Attack Speed = 2s. 200% Attack Speed = 1s.", 2D, true, null),
-    FIRE_DAMAGE("Fire Damage", ChatColor.YELLOW, "Sets enemies on fire.", 0, false, null),
-    LIFE_STEAL("Life Steal", ChatColor.YELLOW, "Heals for a percentage of damage dealt.", 0, true, null),
-    HEALTH("Health", ChatColor.BLUE, "Increases maximum health.", 20, false, null),
-    REGENERATION("Regeneration", ChatColor.BLUE, "Increases health regained during natural regeneration.", 0, false, null),
-    ARMOR("Armor", ChatColor.BLUE, "Protects from a percentage of damage received.", 0, true, null),
-    ARMOR_PENETRATION("Armor Penetration", ChatColor.YELLOW, "Percentage of enemy armor that is ignored.", 0, true, null),
-    DAMAGE_REFLECT("Damage Reflect", ChatColor.BLUE, "Percentage of damage taken reflected back to enemy.", 0, true, null),
-    EVASION("Evasion", ChatColor.BLUE, "Chance to dodge an attack entirely.", 0, true, null),
-    BLOCK("Block", ChatColor.BLUE, "Percentage of damage taken to reduce when blocking.", 0, true, null),
-    PARRY("Parry Chance", ChatColor.BLUE, "Chance to take no damage and reflect some damage when blocking.", 0, true, null);
+    HEALTH("Health", ChatColor.BLUE, "Increases maximum health.", 20, false),
+    ARMOR("Armor", ChatColor.BLUE, "Protects from a percentage of damage received.", 0, true),
+    REGENERATION("Regeneration", ChatColor.BLUE, "Increases health regained during natural regeneration.", 0, false),
+    EVASION("Evasion", ChatColor.BLUE, "Chance to dodge an attack entirely.", 0, true),
+    DAMAGE_REFLECT("Damage Reflect", ChatColor.BLUE, "Percentage of damage taken reflected back to enemy.", 0, true),
+    BLOCK("Block", ChatColor.BLUE, "Percentage of damage taken to reduce when blocking.", 0, true),
+    PARRY("Parry Chance", ChatColor.BLUE, "Chance to take no damage and reflect some damage when blocking.", 0, true),
+    MELEE_DAMAGE("Melee Damage", ChatColor.YELLOW, "Maximum damage dealt by melee attacks", 1, false),
+    RANGED_DAMAGE("Ranged Damage", ChatColor.YELLOW, "Maximum damage dealt by bow", 1, false),
+    ATTACK_SPEED("Attack Speed", ChatColor.YELLOW, "100% Attack Speed = 2s. 200% Attack Speed = 1s.", 2D, true),
+    ARMOR_PENETRATION("Armor Penetration", ChatColor.YELLOW, "Percentage of enemy armor that is ignored.", 0, true),
+    CRITICAL_RATE("Critical Rate", ChatColor.YELLOW, "Chance of landing a critical strike", 0, true),
+    CRITICAL_DAMAGE("Critical Damage", ChatColor.YELLOW, "100% Critical Damage = no bonus. 200% Critical Damage = 2x.", 1.5D, true),
+    FIRE_DAMAGE("Fire Damage", ChatColor.YELLOW, "Sets enemies on fire.", 0, false),
+    LIFE_STEAL("Life Steal", ChatColor.YELLOW, "Heals for a percentage of damage dealt.", 0, true);
 
     private final String name;
     private final ChatColor displayColor;
     private final String description;
     private final double baseValue;
     private final boolean percentage;
-    private final Sound sound;
 
-    private StrifeAttribute(String name, ChatColor displayColor, String description, double baseValue, boolean percentage, Sound sound) {
+    private StrifeAttribute(String name, ChatColor displayColor, String description, double baseValue, boolean percentage) {
         this.name = name;
         this.displayColor = displayColor;
         this.description = description;
         this.baseValue = baseValue;
         this.percentage = percentage;
-        this.sound = sound;
     }
 
     public String getName() {
@@ -60,10 +57,6 @@ public enum StrifeAttribute {
 
     public boolean isPercentage() {
         return percentage;
-    }
-
-    public Sound getSound() {
-        return sound;
     }
 
     public static StrifeAttribute fromName(String s) {
