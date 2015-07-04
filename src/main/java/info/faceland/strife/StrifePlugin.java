21eba111f8e9d9680b703ca529228703ccddb551
Richard Harrah <topplethenunnery@gmail.com>
2015-07-04 13:27:06 -0400

    converting back to maven

--- src/main/java/info/faceland/strife/StrifePlugin.java
+++ src/main/java/info/faceland/strife/StrifePlugin.java
@@ -1,42 +1,42 @@
-/*
- * This file is part of Strife, licensed under the ISC License.
+/**
+ * The MIT License
+ * Copyright (c) 2015 Teal Cube Games
  *
- * Copyright (c) 2014 Richard Harrah
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
  *
- * Permission to use, copy, modify, and/or distribute this software for any purpose with or without fee is hereby granted,
- * provided that the above copyright notice and this permission notice appear in all copies.
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES WITH REGARD TO THIS SOFTWARE INCLUDING ALL
- * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY SPECIAL, DIRECT,
- * INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
- * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE OR PERFORMANCE OF
- * THIS SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
  */
 package info.faceland.strife;
 
 import com.comphenix.xp.lookup.LevelingRate;
+import com.tealcube.minecraft.bukkit.config.MasterConfiguration;
+import com.tealcube.minecraft.bukkit.config.VersionedConfiguration;
+import com.tealcube.minecraft.bukkit.config.VersionedSmartYamlConfiguration;
 import com.tealcube.minecraft.bukkit.facecore.logging.PluginLogger;
 import com.tealcube.minecraft.bukkit.facecore.plugin.FacePlugin;
-import com.tealcube.minecraft.bukkit.facecore.shade.amp.ampmenus.MenuListener;
-import com.tealcube.minecraft.bukkit.facecore.shade.config.MasterConfiguration;
-import com.tealcube.minecraft.bukkit.facecore.shade.config.VersionedSmartConfiguration;
-import com.tealcube.minecraft.bukkit.facecore.shade.config.VersionedSmartYamlConfiguration;
-import com.tealcube.minecraft.bukkit.kern.methodcommand.CommandHandler;
 import com.tealcube.minecraft.bukkit.kern.objecthunter.exp4j.Expression;
 import com.tealcube.minecraft.bukkit.kern.objecthunter.exp4j.ExpressionBuilder;
-
 import info.faceland.beast.BeastPlugin;
 import info.faceland.strife.attributes.StrifeAttribute;
 import info.faceland.strife.commands.AttributesCommand;
 import info.faceland.strife.commands.LevelUpCommand;
 import info.faceland.strife.commands.StrifeCommand;
 import info.faceland.strife.data.Champion;
-import info.faceland.strife.listeners.BullionListener;
-import info.faceland.strife.listeners.CombatListener;
-import info.faceland.strife.listeners.DataListener;
-import info.faceland.strife.listeners.ExperienceListener;
-import info.faceland.strife.listeners.HealthListener;
-import info.faceland.strife.listeners.LootListener;
+import info.faceland.strife.listeners.*;
 import info.faceland.strife.managers.ChampionManager;
 import info.faceland.strife.managers.StrifeStatManager;
 import info.faceland.strife.menus.LevelupMenu;
@@ -47,19 +47,16 @@ import info.faceland.strife.storage.JsonDataStorage;
 import info.faceland.strife.tasks.AttackSpeedTask;
 import info.faceland.strife.tasks.HealthMovementTask;
 import info.faceland.strife.tasks.SaveTask;
-
+import ninja.amp.ampmenus.MenuListener;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.DyeColor;
 import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.event.HandlerList;
+import se.ranzdo.bukkit.methodcommand.CommandHandler;
 
 import java.io.File;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 import java.util.logging.Level;
 
 public class StrifePlugin extends FacePlugin {
@@ -93,10 +90,10 @@ public class StrifePlugin extends FacePlugin {
         debugPrinter = new PluginLogger(this);
         statsYAML = new VersionedSmartYamlConfiguration(new File(getDataFolder(), "stats.yml"),
                                                         getResource("stats.yml"),
-                                                        VersionedSmartConfiguration.VersionUpdateType.BACKUP_AND_UPDATE);
+                                                        VersionedConfiguration.VersionUpdateType.BACKUP_AND_UPDATE);
         configYAML = new VersionedSmartYamlConfiguration(new File(getDataFolder(), "config.yml"),
                                                          getResource("config.yml"),
-                                                         VersionedSmartConfiguration.VersionUpdateType.BACKUP_AND_UPDATE);
+                                                         VersionedConfiguration.VersionUpdateType.BACKUP_AND_UPDATE);
 
         statManager = new StrifeStatManager();
 
