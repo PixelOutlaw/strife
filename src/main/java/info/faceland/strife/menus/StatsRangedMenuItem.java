20e6f993fcec97733f592e70bf0fd9580dd639bd
Kyle Wolfgang Kattrein <kattreinkyle@gmail.com>
2015-08-22 14:09:40 -0400

    Making crits show up under specific scenarios

--- src/main/java/info/faceland/strife/menus/StatsRangedMenuItem.java
+++ src/main/java/info/faceland/strife/menus/StatsRangedMenuItem.java
@@ -67,7 +67,7 @@ public class StatsRangedMenuItem extends MenuItem {
             lore.add(ChatColor.YELLOW + "Snare Chance: " + ChatColor.WHITE + DECIMAL_FORMAT.format(100*valueMap.get(
                 StrifeAttribute.SNARE_CHANCE)) + "%");
         }
-        if (valueMap.get(StrifeAttribute.CRITICAL_RATE) > 0.05) {
+        if (valueMap.get(StrifeAttribute.CRITICAL_RATE) > 0.05 || valueMap.get(StrifeAttribute.CRITICAL_RATE) > 1.3) {
             lore.add(ChatColor.YELLOW + "Critical Strike: " + ChatColor.WHITE + DECIMAL_FORMAT.format(
                 valueMap.get(StrifeAttribute.CRITICAL_RATE)*100) + "% " + ChatColor.GRAY + "(" + DECIMAL_FORMAT.format(
                 valueMap.get(StrifeAttribute.CRITICAL_DAMAGE)*100) + "%)");
