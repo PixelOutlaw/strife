3bcc38cc787e512865763163805f3f3dddc5a3e4
Richard Harrah <topplethenunnery@gmail.com>
2016-02-09 15:07:28 -0500

    reverting some save changes

--- src/main/java/info/faceland/strife/listeners/LoginListener.java
+++ src/main/java/info/faceland/strife/listeners/LoginListener.java
@@ -22,24 +22,16 @@
  */
 package info.faceland.strife.listeners;
 
-import com.tealcube.minecraft.bukkit.facecore.utilities.MessageUtils;
-
 import info.faceland.strife.StrifePlugin;
 import info.faceland.strife.attributes.AttributeHandler;
 import info.faceland.strife.attributes.StrifeAttribute;
 import info.faceland.strife.data.Champion;
 
-import org.bukkit.Bukkit;
-import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
-import org.bukkit.inventory.ItemStack;
-
-import java.util.logging.Level;
 
 public class LoginListener implements Listener {
 
@@ -59,32 +51,6 @@ public class LoginListener implements Listener {
         float speed = 0.2F * (float) perc;
         player.setWalkSpeed(Math.min(Math.max(-1F, speed), 1F));
         player.setFlySpeed(Math.min(Math.max(-1F, speed), 1F));
-    }
-
-    @EventHandler(priority = EventPriority.MONITOR)
-    public void onPlayerQuit(PlayerQuitEvent event) {
-        Player player = event.getPlayer();
-        Champion champion = plugin.getChampionManager().getChampion(player.getUniqueId());
-        Bukkit.getScheduler().scheduleSyncDelayedTask(plugin, new QuitRunnable(plugin, champion), 20L * 10);
-    }
-
-    class QuitRunnable implements Runnable {
-        StrifePlugin plugin;
-        Champion champion;
-
-        QuitRunnable(StrifePlugin plugin, Champion champion) {
-            this.plugin = plugin;
-            this.champion = champion;
-        }
-
-        @Override
-        public void run() {
-            if (champion.getPlayer() != null && champion.getPlayer().isOnline()) {
-                return;
-            }
-            plugin.getStorage().save(champion);
-            plugin.getChampionManager().removeChampion(champion.getUniqueId());
-        }
     }
 
 }
