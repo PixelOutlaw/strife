5708a657410f0dd4361ee174f283fe16144491ef
UltraFaceguy <kattreinkyle@gmail.com>
2018-06-11 19:25:43 -0400

    Cleanup

--- src/main/java/info/faceland/strife/tasks/UniqueParticleTask.java
+++ src/main/java/info/faceland/strife/tasks/UniqueParticleTask.java
@@ -1,24 +1,20 @@
 /**
- * The MIT License
- * Copyright (c) 2015 Teal Cube Games
+ * The MIT License Copyright (c) 2015 Teal Cube Games
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package info.faceland.strife.tasks;
 
@@ -30,28 +26,28 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class UniqueParticleTask extends BukkitRunnable {
 
-    private UniqueEntityManager uniqueEntityManager;
+  private UniqueEntityManager uniqueEntityManager;
 
-    public UniqueParticleTask(UniqueEntityManager uniqueEntityManager) {
-        this.uniqueEntityManager = uniqueEntityManager;
-    }
+  public UniqueParticleTask(UniqueEntityManager uniqueEntityManager) {
+    this.uniqueEntityManager = uniqueEntityManager;
+  }
 
-    @Override
-    public void run() {
-        for (LivingEntity livingEntity : uniqueEntityManager.getLiveUniquesMap().keySet()) {
-            UniqueEntity unique = uniqueEntityManager.getLiveUniquesMap().get(livingEntity);
-            if (unique.getParticle() == null) {
-                continue;
-            }
-            Location location = livingEntity.getLocation();
-            location.getWorld().spawnParticle(
-                    unique.getParticle(),
-                    location,
-                    unique.getParticleCount(),
-                    unique.getParticleRadius(), unique.getParticleRadius(), unique.getParticleRadius(),
-                    0.02f
-            );
-        }
+  @Override
+  public void run() {
+    for (LivingEntity livingEntity : uniqueEntityManager.getLiveUniquesMap().keySet()) {
+      UniqueEntity unique = uniqueEntityManager.getLiveUniquesMap().get(livingEntity);
+      if (unique.getParticle() == null) {
+        continue;
+      }
+      Location location = livingEntity.getLocation();
+      location.getWorld().spawnParticle(
+          unique.getParticle(),
+          location,
+          unique.getParticleCount(),
+          unique.getParticleRadius(), unique.getParticleRadius(), unique.getParticleRadius(),
+          0.02f
+      );
     }
+  }
 
 }
