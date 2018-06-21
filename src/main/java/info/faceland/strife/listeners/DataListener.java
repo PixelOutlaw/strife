377a214ae3bedc76b685b19f39bfde695ed0f4ff
UltraFaceguy <kattreinkyle@gmail.com>
2018-06-21 00:25:58 -0400

    Fixing bleed and barrier persisting after death

--- src/main/java/info/faceland/strife/listeners/DataListener.java
+++ src/main/java/info/faceland/strife/listeners/DataListener.java
@@ -1,24 +1,20 @@
 /**
- * The MIT License
- * Copyright (c) 2015 Teal Cube Games
+ * The MIT License Copyright (c) 2015 Teal Cube Games
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package info.faceland.strife.listeners;
 
@@ -40,51 +36,55 @@ import org.bukkit.event.world.ChunkUnloadEvent;
 
 public class DataListener implements Listener {
 
-    private final StrifePlugin plugin;
+  private final StrifePlugin plugin;
 
-    public DataListener(StrifePlugin plugin) {
-        this.plugin = plugin;
-    }
+  public DataListener(StrifePlugin plugin) {
+    this.plugin = plugin;
+  }
 
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void onPlayerJoin(final PlayerJoinEvent event) {
-        if (!plugin.getChampionManager().hasChampion(event.getPlayer().getUniqueId())) {
-            ChampionSaveData saveData = plugin.getStorage().load(event.getPlayer().getUniqueId());
-            plugin.getChampionManager().addChampion(new Champion(saveData));
-        }
-        if (plugin.getChampionManager().getChampion(event.getPlayer().getUniqueId()).getUnusedStatPoints() > 0) {
-            notifyUnusedPoints(event.getPlayer());
-        }
+  @EventHandler(priority = EventPriority.LOWEST)
+  public void onPlayerJoin(final PlayerJoinEvent event) {
+    if (!plugin.getChampionManager().hasChampion(event.getPlayer().getUniqueId())) {
+      ChampionSaveData saveData = plugin.getStorage().load(event.getPlayer().getUniqueId());
+      plugin.getChampionManager().addChampion(new Champion(saveData));
     }
+    if (plugin.getChampionManager().getChampion(event.getPlayer().getUniqueId())
+        .getUnusedStatPoints() > 0) {
+      notifyUnusedPoints(event.getPlayer());
+    }
+  }
 
-    @EventHandler(priority = EventPriority.LOWEST)
-    public void onEntityDeath(final EntityDeathEvent event) {
-        plugin.getUniqueEntityManager().removeEntity(event.getEntity(), false, true);
-    }
+  @EventHandler(priority = EventPriority.LOWEST)
+  public void onEntityDeath(final EntityDeathEvent event) {
+    plugin.getUniqueEntityManager().removeEntity(event.getEntity(), false, true);
+    plugin.getBleedManager().removeEntity(event.getEntity());
+    plugin.getBarrierManager().removeEntity(event.getEntity());
+  }
 
-    @EventHandler(priority = EventPriority.NORMAL)
-    public void onChunkUnload(ChunkUnloadEvent e){
-        for (Entity ent : e.getChunk().getEntities()) {
-            if (!(ent instanceof LivingEntity)) {
-                continue;
-            }
-            if (plugin.getUniqueEntityManager().getLiveUniquesMap().containsKey(ent)) {
-                plugin.getUniqueEntityManager().removeEntity((LivingEntity) ent, true, false);
-                ent.remove();
-            }
-        }
+  @EventHandler(priority = EventPriority.NORMAL)
+  public void onChunkUnload(ChunkUnloadEvent e) {
+    for (Entity ent : e.getChunk().getEntities()) {
+      if (!(ent instanceof LivingEntity)) {
+        continue;
+      }
+      if (plugin.getUniqueEntityManager().getLiveUniquesMap().containsKey(ent)) {
+        plugin.getUniqueEntityManager().removeEntity((LivingEntity) ent, true, false);
+        ent.remove();
+      }
     }
+  }
 
-    private void notifyUnusedPoints(final Player player) {
-        Bukkit.getScheduler().runTaskLater(plugin, new Runnable() {
-            @Override
-            public void run() {
-                FancyMessage message = new FancyMessage("");
-                message.then("You have unspent levelpoints! ").color(ChatColor.GOLD).then("CLICK HERE")
-                    .command("/levelup").color(ChatColor.WHITE).then(" or use ").color(ChatColor.GOLD)
-                    .then("/levelup").color(ChatColor.WHITE).then(" to spend them and raise your stats!").color
-                    (ChatColor.GOLD).send(player);
-            }
-        }, 20L * 2);
-    }
+  private void notifyUnusedPoints(final Player player) {
+    Bukkit.getScheduler().runTaskLater(plugin, new Runnable() {
+      @Override
+      public void run() {
+        FancyMessage message = new FancyMessage("");
+        message.then("You have unspent levelpoints! ").color(ChatColor.GOLD).then("CLICK HERE")
+            .command("/levelup").color(ChatColor.WHITE).then(" or use ").color(ChatColor.GOLD)
+            .then("/levelup").color(ChatColor.WHITE).then(" to spend them and raise your stats!")
+            .color
+                (ChatColor.GOLD).send(player);
+      }
+    }, 20L * 2);
+  }
 }
