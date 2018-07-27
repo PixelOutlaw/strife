985cbb3285a1b786b89f8ac6a8be506e884e9366
UltraFaceguy <kattreinkyle@gmail.com>
2018-07-26 21:29:25 -0400

    Moving loading to managers

--- src/main/java/info/faceland/strife/managers/EntityEquipmentManager.java
+++ src/main/java/info/faceland/strife/managers/EntityEquipmentManager.java
@@ -1,23 +1,74 @@
 package info.faceland.strife.managers;
 
+import com.tealcube.minecraft.bukkit.TextUtils;
+import info.faceland.strife.util.LogUtil;
+import org.apache.commons.lang3.StringUtils;
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.SkullType;
+import org.bukkit.configuration.ConfigurationSection;
 import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
+import org.bukkit.inventory.meta.SkullMeta;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 public class EntityEquipmentManager {
 
-    private final Map<String, ItemStack> itemMap;
+  private final Map<String, ItemStack> itemMap;
 
-    public EntityEquipmentManager() {
-        this.itemMap = new HashMap<>();
+  public EntityEquipmentManager() {
+    this.itemMap = new HashMap<>();
+  }
+
+  public Map<String, ItemStack> getItemMap() {
+    return itemMap;
+  }
+
+  public ItemStack getItem(String key) {
+    return itemMap.getOrDefault(key, null);
+  }
+
+  public void loadEquipmentItem(String key, ConfigurationSection cs) {
+    Material material;
+    String type = cs.getString("material");
+    try {
+      material = Material.getMaterial(type);
+    } catch (Exception e) {
+      LogUtil.printWarning("Skipping item " + key + " for invalid material");
+      return;
     }
 
-    public Map<String, ItemStack> getItemMap() {
-        return itemMap;
+    ItemStack itemStack;
+    SkullType skullType = null;
+    if (material != Material.SKULL_ITEM) {
+      itemStack = new ItemStack(material);
+    } else {
+      skullType = SkullType.valueOf(cs.getString("skull-type", "ZOMBIE"));
+      itemStack = new ItemStack(Material.SKULL_ITEM, 1, (short) skullType.ordinal());
+    }
+    ItemMeta meta = itemStack.getItemMeta();
+
+    String name = cs.getString("name", "");
+    if (StringUtils.isNotBlank(name)) {
+      meta.setDisplayName(TextUtils.color(name));
     }
 
-    public ItemStack getItem(String key) {
-        return itemMap.getOrDefault(key, null);
+    List<String> lore = new ArrayList<>();
+    for (String line : cs.getStringList("lore")) {
+      lore.add(TextUtils.color(line));
     }
+    meta.setLore(lore);
+
+    itemStack.setItemMeta(meta);
+
+    if (material == Material.SKULL_ITEM && skullType == SkullType.PLAYER) {
+      UUID uuid = UUID.fromString(cs.getString("uuid", "b4064ecc-5508-4848-ae4d-f12fbebd0a52"));
+      SkullMeta skullMeta = (SkullMeta) itemStack.getItemMeta();
+      skullMeta.setOwningPlayer(Bukkit.getOfflinePlayer(uuid));
+      itemStack.setItemMeta(skullMeta);
+    }
+
+    getItemMap().put(key, itemStack);
+  }
 }
--- src/main/java/info/faceland/strife/managers/StrifeStatManager.java
+++ src/main/java/info/faceland/strife/managers/StrifeStatManager.java
@@ -1,155 +1,192 @@
 /**
- * The MIT License
- * Copyright (c) 2015 Teal Cube Games
+ * The MIT License Copyright (c) 2015 Teal Cube Games
  *
- * Permission is hereby granted, free of charge, to any person obtaining a copy
- * of this software and associated documentation files (the "Software"), to deal
- * in the Software without restriction, including without limitation the rights
- * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
- * copies of the Software, and to permit persons to whom the Software is
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"), to deal in the Software without restriction,
+ * including without limitation the rights to use, copy, modify, merge, publish, distribute,
+ * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
  *
- * The above copyright notice and this permission notice shall be included in
- * all copies or substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or
+ * substantial portions of the Software.
  *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
- * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
- * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
- * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
- * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
- * THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
+ * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package info.faceland.strife.managers;
 
 import com.tealcube.minecraft.bukkit.TextUtils;
+import info.faceland.strife.attributes.StrifeAttribute;
 import info.faceland.strife.data.Champion;
 import info.faceland.strife.stats.StrifeStat;
 
 import java.util.*;
 import java.util.Map.Entry;
+import org.bukkit.DyeColor;
+import org.bukkit.configuration.ConfigurationSection;
 
 public class StrifeStatManager {
 
-    private static Map<String, StrifeStat> statMap;
-    private static final String upgradeAvailable = TextUtils.color("&a&lCLICK TO UPGRADE!");
-    private static final String pointCapReached = TextUtils.color("&f&lMaxed Out!");
-    private static final String noUnspentPoints = TextUtils.color("&f&lNo Unspent Points");
+  private static Map<String, StrifeStat> statMap;
+  private static final String upgradeAvailable = TextUtils.color("&a&lCLICK TO UPGRADE!");
+  private static final String pointCapReached = TextUtils.color("&f&lMaxed Out!");
+  private static final String noUnspentPoints = TextUtils.color("&f&lNo Unspent Points");
 
-    public StrifeStatManager() {
-        statMap = new LinkedHashMap<>();
+  public StrifeStatManager() {
+    statMap = new LinkedHashMap<>();
+  }
+
+  public StrifeStat getStat(String name) {
+    if (statMap.containsKey(name)) {
+      return statMap.get(name);
+    }
+    return null;
+  }
+
+  public void addStat(StrifeStat stat) {
+    if (!statMap.containsKey(stat.getKey())) {
+      statMap.put(stat.getKey(), stat);
+    }
+  }
+
+  public void removeStat(String key) {
+    if (statMap.containsKey(key)) {
+      statMap.remove(key);
+    }
+  }
+
+  public List<StrifeStat> getStats() {
+    return new ArrayList<>(statMap.values());
+  }
+
+  public StrifeStat getStatByName(String name) {
+    for (StrifeStat stat : statMap.values()) {
+      if (stat.getKey().equalsIgnoreCase(name) || stat.getName().equalsIgnoreCase(name)) {
+        return stat;
+      }
+    }
+    return null;
+  }
+
+  public int getStatCap(StrifeStat stat, Champion champion) {
+
+    int statCap = stat.getMaxCap();
+    if (stat.getLevelsToRaiseCap() > 0) {
+      double levelCap = stat.getStartCap() + (double) champion.getPlayer().getLevel() / stat
+          .getLevelsToRaiseCap();
+      statCap = Math.max(stat.getStartCap(), (int) Math.floor(levelCap));
     }
 
-    public StrifeStat getStat(String name) {
-        if (statMap.containsKey(name)) {
-            return statMap.get(name);
-        }
-        return null;
+    for (Entry<String, Integer> baseEntry : stat.getBaseStatRequirements().entrySet()) {
+      StrifeStat requirementStat = getStat(baseEntry.getKey());
+      int requirementStatValue = champion.getLevel(requirementStat);
+      int unlockRequirement = baseEntry.getValue();
+      int statIncrement = stat.getStatIncreaseIncrements().get(baseEntry.getKey());
+
+      if (requirementStatValue < unlockRequirement) {
+        return 0;
+      }
+
+      int newStatCap = stat.getStartCap();
+      newStatCap += (requirementStatValue - unlockRequirement) / statIncrement;
+      statCap = Math.min(statCap, newStatCap);
     }
+    return Math.min(statCap, stat.getMaxCap());
+  }
 
-    public void addStat(StrifeStat stat) {
-        if (!statMap.containsKey(stat.getKey())) {
-            statMap.put(stat.getKey(), stat);
-        }
+  public List<String> generateRequirementString(StrifeStat stat, Champion champion, int cap) {
+    List<String> requirementList = new ArrayList<>();
+
+    int levelRequirement = getLevelRequirement(stat, champion);
+    int allocatedPoints = champion.getLevel(stat);
+    if (levelRequirement > champion.getPlayer().getLevel() && allocatedPoints < stat.getMaxCap() &&
+        allocatedPoints == cap) {
+      requirementList.add(increaseString("Level", levelRequirement));
     }
-
-    public void removeStat(String key) {
-        if (statMap.containsKey(key)) {
-            statMap.remove(key);
-        }
+    for (Entry<String, Integer> entry : stat.getBaseStatRequirements().entrySet()) {
+      int statRequirement = getStatRequirement(stat, entry.getKey(), champion);
+      StrifeStat requirementStat = getStat(entry.getKey());
+      if (allocatedPoints < stat.getMaxCap() && allocatedPoints == cap &&
+          champion.getLevel(requirementStat) < statRequirement) {
+        requirementList.add(increaseString(requirementStat.getName(), statRequirement));
+      }
     }
-
-    public List<StrifeStat> getStats() {
-        return new ArrayList<>(statMap.values());
+    if (requirementList.isEmpty()) {
+      if (allocatedPoints == stat.getMaxCap()) {
+        requirementList.add(pointCapReached);
+      } else if (champion.getUnusedStatPoints() == 0) {
+        requirementList.add(noUnspentPoints);
+      } else {
+        requirementList.add(upgradeAvailable);
+      }
     }
+    return requirementList;
+  }
 
-    public StrifeStat getStatByName(String name) {
-        for (StrifeStat stat : statMap.values()) {
-            if (stat.getKey().equalsIgnoreCase(name) || stat.getName().equalsIgnoreCase(name)) {
-                return stat;
-            }
-        }
-        return null;
+  private String increaseString(String name, int value) {
+    return TextUtils.color("&f&lCap Increase: " + name + " " + value);
+  }
+
+  private int getLevelRequirement(StrifeStat stat, Champion champion) {
+    if (stat.getLevelsToRaiseCap() <= 0) {
+      return 0;
     }
+    int requirementIncrease = champion.getPlayer().getLevel() / stat.getLevelsToRaiseCap();
+    return stat.getLevelsToRaiseCap() + requirementIncrease * stat.getLevelsToRaiseCap();
+  }
 
-    public int getStatCap(StrifeStat stat, Champion champion) {
-
-        int statCap = stat.getMaxCap();
-        if (stat.getLevelsToRaiseCap() > 0) {
-            double levelCap = stat.getStartCap() + (double) champion.getPlayer().getLevel() / stat.getLevelsToRaiseCap();
-            statCap = Math.max(stat.getStartCap(), (int) Math.floor(levelCap));
-        }
-
-        for (Entry<String, Integer> baseEntry : stat.getBaseStatRequirements().entrySet()) {
-            StrifeStat requirementStat = getStat(baseEntry.getKey());
-            int requirementStatValue = champion.getLevel(requirementStat);
-            int unlockRequirement = baseEntry.getValue();
-            int statIncrement = stat.getStatIncreaseIncrements().get(baseEntry.getKey());
-
-            if (requirementStatValue < unlockRequirement) {
-                return 0;
-            }
-
-            int newStatCap = stat.getStartCap();
-            newStatCap += (requirementStatValue - unlockRequirement) / statIncrement;
-            statCap = Math.min(statCap, newStatCap);
-        }
-        return Math.min(statCap, stat.getMaxCap());
+  private int getStatRequirement(StrifeStat stat, String requiredStatName, Champion champion) {
+    StrifeStat requirementStat = getStat(requiredStatName);
+    int baseRequirement = stat.getBaseStatRequirements().get(requiredStatName);
+    int statIncrement = stat.getStatIncreaseIncrements().get(requiredStatName);
+    if (baseRequirement > champion.getLevel(requirementStat)) {
+      return stat.getBaseStatRequirements().get(requiredStatName);
     }
-
-    public List<String> generateRequirementString(StrifeStat stat, Champion champion, int cap) {
-        List<String> requirementList = new ArrayList<>();
-
-        int levelRequirement = getLevelRequirement(stat, champion);
-        int allocatedPoints = champion.getLevel(stat);
-        if (levelRequirement > champion.getPlayer().getLevel() && allocatedPoints < stat.getMaxCap() &&
-            allocatedPoints == cap) {
-            requirementList.add(increaseString("Level", levelRequirement));
-        }
-        for (Entry<String, Integer> entry : stat.getBaseStatRequirements().entrySet()) {
-            int statRequirement = getStatRequirement(stat, entry.getKey(), champion);
-            StrifeStat requirementStat = getStat(entry.getKey());
-            if (allocatedPoints < stat.getMaxCap() && allocatedPoints == cap &&
-                champion.getLevel(requirementStat) < statRequirement) {
-                requirementList.add(increaseString(requirementStat.getName(), statRequirement));
-            }
-        }
-        if (requirementList.isEmpty()) {
-            if (allocatedPoints == stat.getMaxCap()) {
-                requirementList.add(pointCapReached);
-            } else if (champion.getUnusedStatPoints() == 0) {
-                requirementList.add(noUnspentPoints);
-            } else {
-                requirementList.add(upgradeAvailable);
-            }
-        }
-        return requirementList;
+    if (baseRequirement + statIncrement * champion.getLevel(stat) <= champion
+        .getLevel(requirementStat)) {
+      return -1;
     }
+    int nextRank = (champion.getLevel(requirementStat) - baseRequirement) / statIncrement;
+    return stat.getBaseStatRequirements().get(requiredStatName) + (nextRank + 1) * statIncrement;
+  }
 
-    private String increaseString(String name, int value) {
-        return TextUtils.color("&f&lCap Increase: " + name + " " + value);
+  public void loadStat(String key, ConfigurationSection cs) {
+    StrifeStat stat = new StrifeStat(key);
+    stat.setName(cs.getString("name"));
+    stat.setDescription(cs.getStringList("description"));
+    stat.setDyeColor(DyeColor.valueOf(cs.getString("dye-color", "WHITE")));
+    stat.setSlot(cs.getInt("slot"));
+    stat.setStartCap(cs.getInt("starting-cap", 0));
+    stat.setMaxCap(cs.getInt("maximum-cap", 100));
+    stat.setLevelsToRaiseCap(cs.getInt("levels-to-raise-cap", -1));
+    Map<String, Integer> baseStatRequirements = new HashMap<>();
+    if (cs.isConfigurationSection("base-attribute-requirements")) {
+      ConfigurationSection reqs = cs.getConfigurationSection("base-attribute-requirements");
+      for (String k : reqs.getKeys(false)) {
+        baseStatRequirements.put(k, reqs.getInt(k));
+      }
     }
-
-    private int getLevelRequirement(StrifeStat stat, Champion champion) {
-        if (stat.getLevelsToRaiseCap() <= 0) {
-            return 0;
-        }
-        int requirementIncrease = champion.getPlayer().getLevel() / stat.getLevelsToRaiseCap();
-        return stat.getLevelsToRaiseCap() + requirementIncrease * stat.getLevelsToRaiseCap();
+    Map<String, Integer> raiseStatCapAttributes = new HashMap<>();
+    if (cs.isConfigurationSection("attributes-to-raise-cap")) {
+      ConfigurationSection raiseReqs = cs.getConfigurationSection("attributes-to-raise-cap");
+      for (String k : raiseReqs.getKeys(false)) {
+        raiseStatCapAttributes.put(k, raiseReqs.getInt(k));
+      }
     }
-
-    private int getStatRequirement(StrifeStat stat, String requiredStatName, Champion champion) {
-        StrifeStat requirementStat = getStat(requiredStatName);
-        int baseRequirement = stat.getBaseStatRequirements().get(requiredStatName);
-        int statIncrement = stat.getStatIncreaseIncrements().get(requiredStatName);
-        if (baseRequirement > champion.getLevel(requirementStat)) {
-            return stat.getBaseStatRequirements().get(requiredStatName);
-        }
-        if (baseRequirement + statIncrement * champion.getLevel(stat) <= champion.getLevel(requirementStat)) {
-            return -1;
-        }
-        int nextRank = (champion.getLevel(requirementStat) - baseRequirement) / statIncrement;
-        return stat.getBaseStatRequirements().get(requiredStatName) + (nextRank + 1) * statIncrement;
+    Map<StrifeAttribute, Double> attributeMap = new HashMap<>();
+    if (cs.isConfigurationSection("attributes")) {
+      ConfigurationSection attrCS = cs.getConfigurationSection("attributes");
+      for (String k : attrCS.getKeys(false)) {
+        StrifeAttribute attr = StrifeAttribute.valueOf(k);
+        attributeMap.put(attr, attrCS.getDouble(k));
+      }
     }
-
+    stat.setStatIncreaseIncrements(raiseStatCapAttributes);
+    stat.setBaseStatRequirements(baseStatRequirements);
+    stat.setAttributeMap(attributeMap);
+    addStat(stat);
+  }
 }
