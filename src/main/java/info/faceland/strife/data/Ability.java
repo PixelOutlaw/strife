6e79d832fea9fbff095ba98b051acce0d5ad733f
UltraFaceguy <kattreinkyle@gmail.com>
2019-02-16 00:39:52 -0500

    Conditions on ability object

--- src/main/java/info/faceland/strife/data/Ability.java
+++ src/main/java/info/faceland/strife/data/Ability.java
@@ -1,6 +1,8 @@
 package info.faceland.strife.data;
 
+import info.faceland.strife.data.condition.Condition;
 import info.faceland.strife.data.effects.Effect;
+import java.util.ArrayList;
 import java.util.List;
 
 public class Ability {
@@ -12,9 +14,10 @@ public class Ability {
   private final List<Effect> effects;
   private final int cooldown;
   private final boolean displayCd;
+  private final List<Condition> conditions;
 
   public Ability(String id, String name, List<Effect> effects, TargetType targetType, double range,
-      int cooldown, boolean displayCd) {
+      int cooldown, boolean displayCd, List<Condition> conditions) {
     this.id = id;
     this.name = name;
     this.cooldown = cooldown;
@@ -22,6 +25,7 @@ public class Ability {
     this.targetType = targetType;
     this.range = range;
     this.displayCd = displayCd;
+    this.conditions = conditions;
   }
 
   public String getId() {
@@ -50,6 +54,10 @@ public class Ability {
 
   public boolean isDisplayCd() {
     return displayCd;
+  }
+
+  public List<Condition> getConditions() {
+    return conditions;
   }
 
   public enum TargetType {
