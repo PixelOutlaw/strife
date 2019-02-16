cdd0abcc622ffe4f27de2dd9767234d2d6e49f76
UltraFaceguy <kattreinkyle@gmail.com>
2019-02-16 00:39:24 -0500

    LoreAbilities can now be selected via ID

--- src/main/java/info/faceland/strife/managers/LoreAbilityManager.java
+++ src/main/java/info/faceland/strife/managers/LoreAbilityManager.java
@@ -3,7 +3,9 @@ package info.faceland.strife.managers;
 import com.tealcube.minecraft.bukkit.TextUtils;
 import com.tealcube.minecraft.bukkit.shade.apache.commons.lang3.StringUtils;
 import info.faceland.strife.data.Ability;
+import info.faceland.strife.data.AttributedEntity;
 import info.faceland.strife.data.LoreAbility;
+import info.faceland.strife.data.effects.Effect;
 import info.faceland.strife.util.LogUtil;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -15,12 +17,16 @@ import org.bukkit.inventory.ItemStack;
 
 public class LoreAbilityManager {
 
-  private final Map<String, LoreAbility> loreAbilityMap;
+  private final Map<String, LoreAbility> loreStringToAbilityMap;
+  private final Map<String, LoreAbility> loreIdToAbilityMap;
   private final AbilityManager abilityManager;
+  private final EffectManager effectManager;
 
-  public LoreAbilityManager(AbilityManager abilityManager) {
-    this.loreAbilityMap = new HashMap<>();
+  public LoreAbilityManager(AbilityManager abilityManager, EffectManager effectManager) {
+    this.loreStringToAbilityMap = new HashMap<>();
+    this.loreIdToAbilityMap = new HashMap<>();
     this.abilityManager = abilityManager;
+    this.effectManager = effectManager;
   }
 
   public List<LoreAbility> getLoreAbilitiesFromItem(ItemStack stack) {
@@ -64,25 +70,48 @@ public class LoreAbilityManager {
     }
 
     Ability ability = abilityManager.getAbility(cs.getString("ability-name", null));
-    if (ability == null) {
-      LogUtil.printError("Failed to load " + key + ". No valid ability defined!");
+    List<String> stringEffects = cs.getStringList("effects");
+    List<Effect> effectList = new ArrayList<>();
+    for (String s : stringEffects) {
+      Effect effect = effectManager.getEffect(s);
+      if (effect != null && !effectList.contains(effect)) {
+        effectList.add(effect);
+      }
+    }
+    if (ability == null && effectList.isEmpty()) {
+      LogUtil.printError("Failed to load " + key + ". No valid ability and no effects defined!");
       return;
     }
-
     List<String> description = TextUtils.color(cs.getStringList("description"));
-
     boolean singleTarget = cs.getBoolean("single-target", true);
-    loreAbilityMap.put(triggerText,
-        new LoreAbility(triggerType, triggerText, ability, singleTarget, description));
+    LoreAbility loreAbility = new LoreAbility(triggerType, triggerText, ability, singleTarget,
+        description);
+    for (Effect e : effectList) {
+      loreAbility.addEffect(e);
+    }
+    loreStringToAbilityMap.put(triggerText, loreAbility);
+    loreIdToAbilityMap.put(key, loreAbility);
     LogUtil.printInfo("Loaded lore ability " + key + " successfully.");
   }
 
-  public LoreAbility getLoreAbilityFromString(String loreString) {
-    return loreAbilityMap.getOrDefault(loreString, null);
+  public void applyLoreAbility(LoreAbility la, AttributedEntity caster, AttributedEntity target) {
+    if (la.getAbility() != null) {
+      abilityManager.execute(la.getAbility(), caster, target);
+    }
+    for (Effect effect : la.getEffects()) {
+      effect.execute(caster, target);
+    }
   }
 
-  public Map<String, LoreAbility> getLoreAbilityMap() {
-    return loreAbilityMap;
+  public LoreAbility getLoreAbilityFromString(String loreString) {
+    return loreStringToAbilityMap.getOrDefault(loreString, null);
+  }
+  public LoreAbility getLoreAbilityFromId(String id) {
+    return loreIdToAbilityMap.getOrDefault(id, null);
+  }
+
+  public Map<String, LoreAbility> getLoreStringToAbilityMap() {
+    return loreStringToAbilityMap;
   }
 
   public enum TriggerType {
