5d3d340f6c71be53b76fa43eb9c69060a0e24f53
UltraFaceguy <kattreinkyle@gmail.com>
2019-06-28 14:51:01 -0400

    More traits and trait changes

--- src/main/java/info/faceland/strife/attributes/StrifeTrait.java
+++ src/main/java/info/faceland/strife/attributes/StrifeTrait.java
@@ -5,10 +5,11 @@ import java.util.Map;
 
 public enum StrifeTrait {
 
-  EXPLOSIVE_PROJECTILES("Projectile Type: Ghast Ball"),
-  SNOWBALL_PROJECTILES("Projectile Type: Snowball"),
-  FIREBALL_PROJECTILES("Projectile Type: Fireball"),
-  WITHER_SKULL_PROJECTILES("Projectile Type: Wither Skull"),
+  EXPLOSIVE_PROJECTILES("Projectiles Are Ghast Balls"),
+  SNOWBALL_PROJECTILES("Projectiles Are Snowballs"),
+  FIREBALL_PROJECTILES("Projectiles Are Fireballs"),
+  WITHER_SKULL_PROJECTILES("Projectiles Are Wither Skulls"),
+  NO_GRAVITY_PROJECTILES("Projectiles Have No Gravity"),
   ELEMENTAL_CRITS("Elemental Damage Can Critically Strike"),
   NO_HEALTH_REGEN("Health Cannot Be Regenerated"),
   NO_BARRIER_ALLOWED("Maximum Barrier Is Always Zero"),
