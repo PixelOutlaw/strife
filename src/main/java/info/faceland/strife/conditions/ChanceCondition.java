2d232869d6c2939ed07b21d7274f73593e49cc5f
UltraFaceguy <kattreinkyle@gmail.com>
2019-06-28 10:22:16 -0400

    Chance condition can now scale with attributes

--- src/main/java/info/faceland/strife/conditions/ChanceCondition.java
+++ src/main/java/info/faceland/strife/conditions/ChanceCondition.java
@@ -1,20 +1,27 @@
 package info.faceland.strife.conditions;
 
+import info.faceland.strife.attributes.StrifeAttribute;
 import info.faceland.strife.data.AttributedEntity;
 import info.faceland.strife.util.DamageUtil;
+import java.util.HashMap;
+import java.util.Map;
 import org.bukkit.potion.PotionEffectType;
 
 public class ChanceCondition implements Condition {
 
   private final double chance;
-  private final Comparison comparison;
+  private final Map<StrifeAttribute, Double> statMults = new HashMap<>();
 
-  public ChanceCondition(Comparison comparison, double chance) {
-    this.comparison = comparison;
+  public ChanceCondition(double chance) {
     this.chance = chance;
   }
 
   public boolean isMet(AttributedEntity attacker, AttributedEntity target) {
-    return DamageUtil.rollBool(chance, attacker.getEntity().hasPotionEffect(PotionEffectType.LUCK));
+    double bonusChance = 1;
+    for (StrifeAttribute attr : statMults.keySet()) {
+      bonusChance += attacker.getAttribute(attr) * statMults.get(attr);
+    }
+    return DamageUtil.rollBool(chance * bonusChance,
+        attacker.getEntity().hasPotionEffect(PotionEffectType.LUCK));
   }
 }
