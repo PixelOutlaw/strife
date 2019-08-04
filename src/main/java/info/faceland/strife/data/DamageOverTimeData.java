bc70243733aca86f54307c55230b41a31b2b0949
UltraFaceguy <kattreinkyle@gmail.com>
2019-08-04 14:10:00 -0400

    Adding object so DoT actually works

---
+++ src/main/java/info/faceland/strife/data/DamageOverTimeData.java
@@ -0,0 +1,26 @@
+package info.faceland.strife.data;
+
+public class DamageOverTimeData {
+  private int storedDamage;
+  private long lastAddition;
+
+  public DamageOverTimeData() {
+    this.lastAddition = System.currentTimeMillis();
+  }
+
+  public int getStoredDamage() {
+    return storedDamage;
+  }
+
+  public void setStoredDamage(int storedDamage) {
+    this.storedDamage = storedDamage;
+  }
+
+  public long getLastAddition() {
+    return lastAddition;
+  }
+
+  public void setLastAddition(long lastAddition) {
+    this.lastAddition = lastAddition;
+  }
+}
