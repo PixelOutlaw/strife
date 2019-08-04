27917338ee5c26890dea2af2e40630333f6ad620
UltraFaceguy <kattreinkyle@gmail.com>
2019-08-04 14:09:31 -0400

    Refactoring DOT logic to allow make them less annoying to have, and not useful for blocking damage...

--- src/main/java/info/faceland/strife/listeners/combat/DOTListener.java
+++ src/main/java/info/faceland/strife/listeners/combat/DOTListener.java
@@ -21,20 +21,28 @@ package info.faceland.strife.listeners.combat;
 import static info.faceland.strife.util.DamageUtil.getResistPotionMult;
 
 import info.faceland.strife.StrifePlugin;
+import info.faceland.strife.data.DamageOverTimeData;
 import info.faceland.strife.data.StrifeMob;
 import info.faceland.strife.util.StatUtil;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 import org.bukkit.Sound;
-import org.bukkit.attribute.Attribute;
 import org.bukkit.entity.LivingEntity;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageEvent;
-import org.bukkit.event.entity.EntityDamageEvent.DamageCause;
+import org.bukkit.potion.PotionEffectType;
 
 public class DOTListener implements Listener {
 
   private final StrifePlugin plugin;
+  private final Map<LivingEntity, DamageOverTimeData> poisonMap = new ConcurrentHashMap<>();
+  private final Map<LivingEntity, DamageOverTimeData> witherMap = new ConcurrentHashMap<>();
+
+  private static final long MAX_DOT_MS = 1000;
+  private static final long DOT_PRUNE_TIME_MINIMUM = 10000;
+  private long lastPruneStamp = System.currentTimeMillis();
 
   public DOTListener(StrifePlugin plugin) {
     this.plugin = plugin;
@@ -42,51 +50,101 @@ public class DOTListener implements Listener {
 
   @EventHandler(priority = EventPriority.MONITOR)
   public void onEntityDOTEvent(EntityDamageEvent event) {
-    if (!(event.getEntity() instanceof LivingEntity)) {
+    if (!(event.getEntity() instanceof LivingEntity) || event.isCancelled()) {
       return;
     }
-    if (event.getCause() == EntityDamageEvent.DamageCause.ENTITY_ATTACK) {
-      return;
-    }
-    LivingEntity entity = (LivingEntity) event.getEntity();
-    StrifeMob statEntity = plugin.getStrifeMobManager().getStatMob(entity);
 
-    if (event.getCause() == DamageCause.FIRE_TICK) {
-      double damage = (1 + entity.getHealth() * 0.04) * getResistPotionMult(entity) * (1
-          - StatUtil.getFireResist(statEntity) / 100);
-      damage = plugin.getBarrierManager().damageBarrier(statEntity, damage);
-      entity.getWorld().playSound(entity.getLocation(), Sound.ITEM_FLINTANDSTEEL_USE, 0.5f, 0.75f);
-      entity.damage(damage);
-      event.setCancelled(true);
+    LivingEntity entity = (LivingEntity) event.getEntity();
+    double damage;
+    switch (event.getCause()) {
+      case ENTITY_ATTACK:
+        return;
+      case FIRE_TICK:
+        StrifeMob statEntity = plugin.getStrifeMobManager().getStatMob(entity);
+        damage = 1 + entity.getHealth() * 0.04;
+        damage *= getResistPotionMult(entity);
+        damage *= 1 - StatUtil.getFireResist(statEntity) / 100;
+        damage = plugin.getBarrierManager().damageBarrier(statEntity, damage);
+        entity.getWorld().playSound(entity.getLocation(), Sound.ITEM_FIRECHARGE_USE, 0.8f, 0.5f);
+        dealDirectDamage(entity, damage);
+        event.setCancelled(true);
+        break;
+      case FIRE:
+      case LAVA:
+        StrifeMob statEntity2 = plugin.getStrifeMobManager().getStatMob(entity);
+        damage = 2 + entity.getHealth() * 0.1;
+        damage *= getResistPotionMult(entity);
+        damage *= 1 - StatUtil.getFireResist(statEntity2) / 100;
+        damage = plugin.getBarrierManager().damageBarrier(statEntity2, damage);
+        event.setDamage(damage);
+        break;
+      case WITHER:
+        plugin.getBarrierManager().interruptBarrier(entity);
+        witherMap.putIfAbsent(entity, new DamageOverTimeData());
+        DamageOverTimeData data = witherMap.get(entity);
+        damage = determineDotDamage(data,
+            entity.getPotionEffect(PotionEffectType.WITHER).getAmplifier() + 1);
+        if (damage == 0) {
+          bumpDotData(data, (int) damage);
+        } else {
+          damage = damage + (damage * 0.01 * entity.getMaxHealth());
+          dealDirectDamage(entity, damage);
+        }
+        event.setCancelled(true);
+        break;
+      case POISON:
+        plugin.getBarrierManager().interruptBarrier(entity);
+        poisonMap.putIfAbsent(entity, new DamageOverTimeData());
+        DamageOverTimeData data2 = poisonMap.get(entity);
+        damage = determineDotDamage(data2,
+            entity.getPotionEffect(PotionEffectType.POISON).getAmplifier() + 1);
+        if (damage == 0) {
+          bumpDotData(data2, (int) damage);
+        } else {
+          damage = damage + (damage * 0.01 * entity.getMaxHealth());
+          dealDirectDamage(entity, Math.min(damage, entity.getHealth() - 1));
+        }
+        event.setCancelled(true);
+        break;
+    }
+    pruneInvalidMapEntries();
+  }
+
+  private double determineDotDamage(DamageOverTimeData data, int damage) {
+    if (data.getLastAddition() + MAX_DOT_MS < System.currentTimeMillis()) {
+      double dealtDamage = damage + data.getLastAddition();
+      data.setLastAddition(System.currentTimeMillis());
+      data.setStoredDamage(0);
+      return dealtDamage;
+    }
+    return 0;
+  }
+
+  private void bumpDotData(DamageOverTimeData data, int damage) {
+    data.setStoredDamage(damage + data.getStoredDamage());
+  }
+
+  private void dealDirectDamage(LivingEntity entity, double damage) {
+    int noDamageTicks = entity.getNoDamageTicks();
+    entity.setNoDamageTicks(0);
+    entity.damage(damage);
+    entity.setNoDamageTicks(noDamageTicks);
+  }
+
+  private void pruneInvalidMapEntries() {
+    if (lastPruneStamp + DOT_PRUNE_TIME_MINIMUM > System.currentTimeMillis()) {
       return;
     }
-    if (event.getCause() == DamageCause.FIRE || event.getCause() == DamageCause.LAVA) {
-      double damage = (2 + entity.getHealth() * 0.1) * getResistPotionMult(entity) * (1
-          - StatUtil.getFireResist(statEntity) / 100);
-      damage = plugin.getBarrierManager().damageBarrier(statEntity, damage);
-      event.setDamage(damage);
-      return;
-    }
-    if (event.getCause() == DamageCause.POISON) {
-      plugin.getBarrierManager().interruptBarrier(entity);
-      double damage = 1 + entity.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue() * 0.005;
-      damage *= getResistPotionMult(entity);
-      entity.getWorld().playSound(entity.getLocation(), Sound.ENTITY_SPIDER_STEP, 1.0f, 1.5f);
-      entity.setHealth(Math.max(entity.getHealth() - damage, 1));
-      event.setCancelled(true);
-      return;
-    }
-    if (event.getCause() == DamageCause.WITHER) {
-      plugin.getBarrierManager().interruptBarrier(entity);
-      double damage = 1 + entity.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue() * 0.005;
-      damage *= getResistPotionMult(entity);
-      entity.getWorld().playSound(entity.getLocation(), Sound.ENTITY_WITHER_SHOOT, 0.2f, 0.5f);
-      if (entity.getHealth() > damage) {
-        entity.setHealth(entity.getHealth() - damage);
-      } else {
-        entity.damage(damage);
+    for (LivingEntity le : witherMap.keySet()) {
+      if (!le.isValid()) {
+        witherMap.remove(le);
       }
-      event.setCancelled(true);
     }
+    for (LivingEntity le : poisonMap.keySet()) {
+      if (!le.isValid()) {
+        poisonMap.remove(le);
+      }
+    }
+    lastPruneStamp = System.currentTimeMillis();
   }
 }
