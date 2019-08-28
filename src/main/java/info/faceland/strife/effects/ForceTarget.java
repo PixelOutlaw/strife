d3e8118b7b4196e41c3b7125a7fff0e1e64734c4
UltraFaceguy <kattreinkyle@gmail.com>
2019-08-27 23:04:57 -0400

    Adding field to determine the direction of targeting

--- src/main/java/info/faceland/strife/effects/ForceTarget.java
+++ src/main/java/info/faceland/strife/effects/ForceTarget.java
@@ -1,26 +1,32 @@
 package info.faceland.strife.effects;
 
 import info.faceland.strife.data.StrifeMob;
-import info.faceland.strife.util.LogUtil;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Mob;
 
 public class ForceTarget extends Effect {
 
+  private boolean casterTargetsTarget;
   private boolean overwrite;
 
   @Override
   public void apply(StrifeMob caster, StrifeMob target) {
-    if (caster.getEntity() instanceof Mob) {
-      if (!overwrite && ((Mob) caster.getEntity()).getTarget() != null) {
-        return;
-      }
-      ((Mob) caster.getEntity()).setTarget(target.getEntity());
-    } else {
-      LogUtil.printWarning(caster.getEntity().getName() + " is not a mob. Target failed.");
+    LivingEntity fromTarget = casterTargetsTarget ? caster.getEntity() : target.getEntity();
+    LivingEntity toTarget = casterTargetsTarget ? target.getEntity() : caster.getEntity();
+    if (!(fromTarget instanceof Mob)) {
+      return;
     }
+    if (!overwrite && ((Mob) fromTarget).getTarget() != null) {
+      return;
+    }
+    ((Mob) fromTarget).setTarget(toTarget);
   }
 
   public void setOverwrite(boolean overwrite) {
     this.overwrite = overwrite;
+  }
+
+  public void setCasterTargetsTarget(boolean casterTargetsTarget) {
+    this.casterTargetsTarget = casterTargetsTarget;
   }
 }
