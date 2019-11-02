c2198f77d77cd13ce36f96680ed1719f3f907c13
UltraFaceguy <kattreinkyle@gmail.com>
2019-11-02 15:41:55 -0400

    Entitytype conditions respect disguises

--- src/main/java/land/face/strife/data/conditions/EntityTypeCondition.java
+++ src/main/java/land/face/strife/data/conditions/EntityTypeCondition.java
@@ -3,6 +3,7 @@ package land.face.strife.data.conditions;
 import java.util.Set;
 import land.face.strife.data.StrifeMob;
 import land.face.strife.util.LogUtil;
+import me.libraryaddict.disguise.DisguiseAPI;
 import org.bukkit.entity.EntityType;
 
 public class EntityTypeCondition extends Condition {
@@ -17,12 +18,18 @@ public class EntityTypeCondition extends Condition {
 
   public boolean isMet(StrifeMob attacker, StrifeMob target) {
     LogUtil.printDebug("EntityType condition, type=" + target.getEntity().getType());
+    EntityType type;
+    if (DisguiseAPI.isDisguised(target.getEntity())) {
+      type = DisguiseAPI.getDisguise(target.getEntity()).getType().getEntityType();
+    } else {
+      type = target.getEntity().getType();
+    }
     if (whitelist) {
       LogUtil.printDebug("EntityType condition, whitelist=true, " + types.toString());
-      return types.contains(target.getEntity().getType());
+      return types.contains(type);
     } else {
       LogUtil.printDebug("EntityType condition, whitelist=false, " + types.toString());
-      return !types.contains(target.getEntity().getType());
+      return !types.contains(type);
     }
   }
 
