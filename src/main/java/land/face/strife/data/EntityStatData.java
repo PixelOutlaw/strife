fe782c6c6542d4821e4f8ff6307e17ee9ce30708
UltraFaceguy <kattreinkyle@gmail.com>
2019-11-03 12:30:03 -0500

    missed in previous commit

--- src/main/java/land/face/strife/data/EntityStatData.java
+++ src/main/java/land/face/strife/data/EntityStatData.java
@@ -1,10 +1,13 @@
 package land.face.strife.data;
 
+import com.tealcube.minecraft.bukkit.shade.objecthunter.exp4j.Expression;
 import java.util.HashMap;
 import java.util.Map;
 import land.face.strife.stats.StrifeStat;
 
 public class EntityStatData {
+
+  private Expression baseExpExpr;
   private Map<StrifeStat, Float> baseValueMap;
   private Map<StrifeStat, Float> perLevelMap;
   private Map<StrifeStat, Float> perBonusLevelMap;
@@ -16,9 +19,18 @@ public class EntityStatData {
   }
 
   public EntityStatData(EntityStatData entityStatData) {
+    baseExpExpr = entityStatData.getBaseExpExpr();
     baseValueMap = new HashMap<>(entityStatData.getBaseValueMap());
     perLevelMap = new HashMap<>(entityStatData.getPerLevelMap());
     perBonusLevelMap = new HashMap<>(entityStatData.getPerBonusLevelMap());
+  }
+
+  public Expression getBaseExpExpr() {
+    return baseExpExpr;
+  }
+
+  public void setBaseExpExpr(Expression baseExpExpr) {
+    this.baseExpExpr = baseExpExpr;
   }
 
   public Map<StrifeStat, Float> getBaseValueMap() {
