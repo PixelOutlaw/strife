b7bb16ba1f391678369765327744d95f5c05ea87
UltraFaceguy <kattreinkyle@gmail.com>
2019-11-11 22:52:47 -0500

    Adding flintlock pistols

--- src/main/java/land/face/strife/listeners/ShootListener.java
+++ src/main/java/land/face/strife/listeners/ShootListener.java
@@ -22,11 +22,21 @@ import java.util.Objects;
 import land.face.strife.StrifePlugin;
 import land.face.strife.data.StrifeMob;
 import land.face.strife.data.ability.EntityAbilitySet.TriggerAbilityType;
+import land.face.strife.data.effects.AreaEffect;
+import land.face.strife.data.effects.AreaEffect.AreaType;
+import land.face.strife.data.effects.AreaEffect.TargetingPriority;
+import land.face.strife.data.effects.StandardDamage;
+import land.face.strife.data.effects.StrifeParticle;
+import land.face.strife.data.effects.StrifeParticle.ParticleStyle;
 import land.face.strife.stats.StrifeStat;
+import land.face.strife.util.DamageUtil.AttackType;
+import land.face.strife.util.DamageUtil.OriginLocation;
 import land.face.strife.util.LogUtil;
 import land.face.strife.util.ProjectileUtil;
+import land.face.strife.util.StatUtil;
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.entity.Arrow;
 import org.bukkit.entity.LivingEntity;
@@ -37,26 +47,57 @@ import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.ProjectileHitEvent;
 import org.bukkit.event.entity.ProjectileLaunchEvent;
+import org.bukkit.inventory.ItemStack;
 
 public class ShootListener implements Listener {
 
   private final StrifePlugin plugin;
+  private StrifeParticle flintlockSmoke;
+  private StrifeParticle flintlockFlare;
+  private AreaEffect flintlockHitscan;
 
   public ShootListener(StrifePlugin plugin) {
     this.plugin = plugin;
+    flintlockSmoke = buildFlintlockSmoke();
+    flintlockFlare = buildFlintlockFlare();
+    flintlockHitscan = buildFlintlockHitscan();
   }
 
   @EventHandler(priority = EventPriority.HIGH)
   public void onEntityShoot(ProjectileLaunchEvent event) {
-    if (!(event.getEntity().getShooter() instanceof LivingEntity) || !(event
-        .getEntity() instanceof Arrow)) {
+    if (!(event.getEntity().getShooter() instanceof LivingEntity) ||
+        !(event.getEntity() instanceof Arrow)) {
       return;
     }
 
+    LivingEntity shooter = (LivingEntity) event.getEntity().getShooter();
     StrifeMob mob = plugin.getStrifeMobManager()
         .getStatMob((LivingEntity) event.getEntity().getShooter());
 
     event.setCancelled(true);
+
+    ItemStack weapon = Objects.requireNonNull(((LivingEntity) event.getEntity().getShooter())
+        .getEquipment()).getItemInMainHand();
+    if (weapon.hasItemMeta() && Objects.requireNonNull(weapon.getItemMeta()).hasCustomModelData()
+        && weapon.getItemMeta().getCustomModelData() != 0) {
+      int itemData = weapon.getItemMeta().getCustomModelData();
+      if (weapon.getType() == Material.BOW && itemData > 10999 && itemData < 12000) {
+        if (shooter instanceof Player) {
+          if (((Player) shooter).getCooldown(Material.BOW) > 0) {
+            return;
+          }
+          ((Player) shooter).setCooldown(Material.BOW, (int) (StatUtil.getAttackTime(mob) * 20));
+        }
+        plugin.getEffectManager().execute(flintlockHitscan, mob, mob.getEntity());
+        flintlockSmoke.apply(null, mob);
+        flintlockFlare.apply(null, mob);
+        System.out.println(flintlockFlare.getSpeed());
+        System.out.println(flintlockSmoke.getSpeed());
+        mob.getEntity().getWorld()
+            .playSound(mob.getEntity().getLocation(), Sound.ENTITY_FIREWORK_ROCKET_BLAST, 1f, 0.7f);
+        return;
+      }
+    }
 
     if (mob.getAbilitySet() != null
         && mob.getAbilitySet().getAbilities(TriggerAbilityType.SHOOT) != null) {
@@ -81,8 +122,8 @@ public class ShootListener implements Listener {
     projectiles--;
 
     for (int i = projectiles; i > 0; i--) {
-      ProjectileUtil.createArrow(mob.getEntity(), attackMultiplier, projectileSpeed, randomOffset(projectiles),
-          0.17);
+      ProjectileUtil.createArrow(mob.getEntity(), attackMultiplier, projectileSpeed,
+          randomOffset(projectiles), 0.17);
     }
     mob.getEntity().getWorld()
         .playSound(mob.getEntity().getLocation(), Sound.ENTITY_ARROW_SHOOT, 1f, 1f);
@@ -116,8 +157,8 @@ public class ShootListener implements Listener {
 
     double speedMult = 1 + (pStats.getStat(StrifeStat.PROJECTILE_SPEED) / 100);
 
-    ProjectileUtil.createTrident((Player) event.getEntity().getShooter(), (Trident) event.getEntity(),
-        attackMultiplier, speedMult);
+    ProjectileUtil.createTrident((Player) event.getEntity().getShooter(),
+        (Trident) event.getEntity(), attackMultiplier, speedMult);
 
     player.getWorld().playSound(player.getLocation(), Sound.ITEM_TRIDENT_THROW, 1f, 1f);
     plugin.getSneakManager().tempDisableSneak(player);
@@ -137,8 +178,8 @@ public class ShootListener implements Listener {
     String[] effects = event.getEntity().getMetadata("EFFECT_PROJECTILE").get(0).asString()
         .split("~");
     if (effects.length == 0) {
-      LogUtil
-          .printWarning("A handled GroundProjectile was missing effect meta... something's wrong");
+      LogUtil.printWarning(
+          "A handled GroundProjectile was missing effect meta... something's wrong");
       return;
     }
     Location loc = event.getEntity().getLocation().clone()
@@ -152,4 +193,48 @@ public class ShootListener implements Listener {
   private double randomOffset(double magnitude) {
     return 0.11 + magnitude * 0.005;
   }
+
+  private StrifeParticle buildFlintlockSmoke() {
+    StrifeParticle particle = new StrifeParticle();
+    particle.setFriendly(true);
+    particle.setParticle(Particle.CAMPFIRE_COSY_SMOKE);
+    particle.setParticleOriginLocation(OriginLocation.BELOW_HEAD);
+    particle.setStyle(ParticleStyle.LINE);
+    particle.setSize(2);
+    particle.setQuantity(plugin.getSettings().getInt("config.flintlock.smoke-quantity", 1));
+    particle.setLineOffset(plugin.getSettings().getDouble("config.flintlock.smoke-offset", 1));
+    particle.setSpeed((float) plugin.getSettings().getDouble("config.flintlock.smoke-speed", 2f));
+    particle.setSpread((float) plugin.getSettings().getDouble("config.flintlock.smoke-spread", 2f));
+    return particle;
+  }
+
+  private StrifeParticle buildFlintlockFlare() {
+    StrifeParticle particle = new StrifeParticle();
+    particle.setFriendly(true);
+    particle.setParticle(Particle.FLAME);
+    particle.setParticleOriginLocation(OriginLocation.BELOW_HEAD);
+    particle.setStyle(ParticleStyle.LINE);
+    particle.setSize(1);
+    particle.setQuantity(plugin.getSettings().getInt("config.flintlock.flare-quantity", 1));
+    particle.setLineOffset(plugin.getSettings().getDouble("config.flintlock.flare-offset", 1));
+    particle.setSpeed((float) plugin.getSettings().getDouble("config.flintlock.flare-speed", 1f));
+    particle.setSpread((float) plugin.getSettings().getDouble("config.flintlock.flare-spread", 1f));
+    return particle;
+  }
+
+  private AreaEffect buildFlintlockHitscan() {
+    StandardDamage standardDamage = new StandardDamage();
+    standardDamage.setAttackMultiplier(1.0f);
+    standardDamage.setAttackType(AttackType.RANGED);
+
+    AreaEffect hitscan = new AreaEffect();
+    hitscan.setAreaType(AreaType.LINE);
+    hitscan.setPriority(TargetingPriority.CLOSEST);
+    hitscan.setRange((float) plugin.getSettings().getDouble("config.flintlock.range", 16f));
+    hitscan.setLineOfSight(true);
+    hitscan.setMaxTargets(1);
+    hitscan.getEffects().add(standardDamage);
+    return hitscan;
+  }
+
 }
