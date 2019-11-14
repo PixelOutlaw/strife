5ab0a812f6d084ce89aa266b605e40222a25386d
UltraFaceguy <kattreinkyle@gmail.com>
2019-11-13 19:05:55 -0500

    oh, hi mark

--- src/main/java/land/face/strife/data/effects/TargetingComparators/PercentHealthComparator.java
+++ src/main/java/land/face/strife/data/effects/TargetingComparators/PercentHealthComparator.java
@@ -8,7 +8,7 @@ public class PercentHealthComparator implements Comparator<LivingEntity> {
 
   public int compare(LivingEntity le1, LivingEntity le2) {
     double max1 = le1.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue();
-    double max2 = le1.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue();
+    double max2 = le2.getAttribute(Attribute.GENERIC_MAX_HEALTH).getBaseValue();
     return Double.compare(le1.getHealth() / max1, le2.getHealth() / max2);
   }
 }
