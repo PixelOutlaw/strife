79a4c6e4a425aede0ef4d3c55554db3bb82e7896
UltraFaceguy <kattreinkyle@gmail.com>
2020-03-22 14:17:53 -0400

    Making lightning a location effect

--- src/main/java/land/face/strife/data/effects/Lightning.java
+++ src/main/java/land/face/strife/data/effects/Lightning.java
@@ -1,11 +1,17 @@
 package land.face.strife.data.effects;
 
 import land.face.strife.data.StrifeMob;
+import org.bukkit.Location;
 
-public class Lightning extends Effect {
+public class Lightning extends LocationEffect {
 
   @Override
   public void apply(StrifeMob caster, StrifeMob target) {
-    target.getEntity().getWorld().strikeLightningEffect(target.getEntity().getLocation());
+    applyAtLocation(caster, target.getEntity().getLocation());
+  }
+
+  @Override
+  public void applyAtLocation(StrifeMob caster, Location location) {
+    location.getWorld().strikeLightningEffect(location);
   }
 }
