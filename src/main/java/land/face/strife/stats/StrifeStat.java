b1a54393743aaf7385f60ad69deae20f7e747070
UltraFaceguy <kattreinkyle@gmail.com>
2020-08-17 12:39:24 -0400

    Removing head drop as a stat

--- src/main/java/land/face/strife/stats/StrifeStat.java
+++ src/main/java/land/face/strife/stats/StrifeStat.java
@@ -128,7 +128,6 @@ public enum StrifeStat {
   ITEM_DISCOVERY("Loot Bonus"),
   ITEM_RARITY("Loot Rarity"),
   GOLD_FIND("Gold Bonus"),
-  HEAD_DROP("Head Drop"),
   FISHING_SPEED("Fishing Speed"),
   FISHING_TREASURE("Fishing Treasures"),
   MINING_GEMS("Mining Gems"),
