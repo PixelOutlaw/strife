37ae92fab0f25e77e8c04368de7334e1e60eb56c
UltraFaceguy <kattreinkyle@gmail.com>
2020-08-17 12:49:51 -0400

    Using iterator for particles to prevent fringe issues

--- src/main/java/land/face/strife/tasks/ParticleTask.java
+++ src/main/java/land/face/strife/tasks/ParticleTask.java
@@ -1,20 +1,18 @@
 /**
  * The MIT License Copyright (c) 2015 Teal Cube Games
  * <p>
- * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and
- * associated documentation files (the "Software"), to deal in the Software without restriction,
- * including without limitation the rights to use, copy, modify, merge, publish, distribute,
- * sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is
- * furnished to do so, subject to the following conditions:
+ * Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
+ * documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
+ * rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to
+ * permit persons to whom the Software is furnished to do so, subject to the following conditions:
  * <p>
- * The above copyright notice and this permission notice shall be included in all copies or
- * substantial portions of the Software.
+ * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the
+ * Software.
  * <p>
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT
- * NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
- * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
- * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
- * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE
+ * WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+ * COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  */
 package land.face.strife.tasks;
 
@@ -42,17 +40,21 @@ public class ParticleTask extends BukkitRunnable {
       tick = 1;
     }
 
-    for (LivingEntity entity : continuousParticles.keySet()) {
-      if (!entity.isValid() || continuousParticles.get(entity).isEmpty()) {
-        continuousParticles.remove(entity);
+    Iterator<LivingEntity> cParticle = continuousParticles.keySet().iterator();
+    while ((cParticle.hasNext())) {
+      LivingEntity le = cParticle.next();
+      if (le == null || !le.isValid() || continuousParticles.get(le).isEmpty()) {
+        cParticle.remove();
         continue;
       }
-      doContinuousParticles(entity);
+      doContinuousParticles(le);
     }
 
-    for (LivingEntity le : boundParticles.keySet()) {
-      if (!le.isValid()) {
-        boundParticles.remove(le);
+    Iterator<LivingEntity> iterator = boundParticles.keySet().iterator();
+    while ((iterator.hasNext())) {
+      LivingEntity le = iterator.next();
+      if (le == null || !le.isValid()) {
+        iterator.remove();
         continue;
       }
       StrifeParticle particle = boundParticles.get(le);
