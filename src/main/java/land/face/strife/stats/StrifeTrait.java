5b4cdd2683e63111ae6ee0c7b63184a982e83bc3
Ultrafaceguy <kattreinkyle@gmail.com>
2022-07-29 16:26:48 -0400

    Update traits to use FaceColors

--- src/main/java/land/face/strife/stats/StrifeTrait.java
+++ src/main/java/land/face/strife/stats/StrifeTrait.java
@@ -1,14 +1,10 @@
 package land.face.strife.stats;
 
-import io.pixeloutlaw.minecraft.spigot.garbage.ListExtensionsKt;
-import io.pixeloutlaw.minecraft.spigot.garbage.StringExtensionsKt;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import com.tealcube.minecraft.bukkit.facecore.utilities.PaletteUtil;
 import lombok.Getter;
 import net.md_5.bungee.api.ChatColor;
+
+import java.util.*;
 
 public enum StrifeTrait {
 
@@ -21,38 +17,38 @@ public enum StrifeTrait {
   NO_ENERGY_BASICS("Basic Attacks Cost No Energy"),
   ENERGY_ABSORB("20% Of Damage Taken From Energy"),
   NO_ENERGY_REGEN("Energy Does Not Regenerate"),
-  BLEEDING_EDGE("&6&nPassive Effect - Bleeding Edge",
-      " &eHalf of attack damage taken from",
-      " &elife is instead added as &4Bleeding"),
-  AIR_DASH("&6&nPassive Effect - Air Dash",
-      " &eAir Jumps gain horizontal strength",
-      " &eat the cost of vertical strength"),
-  BARRIER_NO_BURN("&6&nPassive Effect - Heat Shield",
-      " &eWhile you have barrier, &6&nBurning",
-      " &edeals no damage"),
-  STONE_SKIN("&6&nPassive Effect - Stone Skin",
-      " &eReceive 3% less damage per",
-      " &eactive &2Earth Rune"),
-  BLOOD_BOIL("&6&nPassive Effect - Blood Boil",
-      " &eWhen &4Bleeding&e, &eall &cRage &egained",
-      " &eis increased by &f30%"),
-  BLOOD_AND_ICE("&6&nPassive Effect - Blood And Ice",
-      " &eApply 30% more &bFrost &eto &4Bleeding &eenemies",
-      " &eApply 30% more &4Bleed to &bFrosted &eenemies"),
-  OVERSHIELD("&6&nPassive Effect - Overshield",
-      " &eWhen your &fLife &eis full, gain &f30% &eof",
-      " &eyour &fLife Regen. &eas &fBarrier Regen."),
-  ELEMENTAL_CRITS_2("&6&nPassive Effect - Status Strike",
-      " &eCritical strikes trigger &3elemental status",
-      " &einstead of dealing extra damage"),
-  LETHAL_STRIKE("&6&nPassive Effect - Lethal Strike",
-      " &eAttacks with over 100% Critical Chance may",
-      " &etrigger a &c&nLethal Strike&e, doubling critical",
-      " &edamage and triggering &6&nON CRIT&e again"),
-  IRON_SCARS("&6&nPassive Effect - Iron Scars",
-      " &eReceive &f60% &eless bonus damage from",
-      " &ecritical strikes"),
-  SOUL_FLAME("Passive Effect - Blue Flame");
+  BLEEDING_EDGE("|orange||ul|Passive Effect - Bleeding Edge",
+      " |yellow|Half of attack damage taken from",
+      " |yellow|life is instead added as |red|Bleeding"),
+  AIR_DASH("|orange||ul|Passive Effect - Air Dash",
+      " |yellow|Air Jumps gain horizontal strength",
+      " |yellow|at the cost of vertical strength"),
+  BARRIER_NO_BURN("|orange||ul|Passive Effect - Heat Shield",
+      " |yellow|While you have barrier, |orange||ul|Burning",
+      " |yellow|deals no damage"),
+  STONE_SKIN("|orange||ul|Passive Effect - Stone Skin",
+      " |yellow|Receive |white|3% |yellow|less damage per",
+      " |yellow|active |brown|Earth Rune"),
+  BLOOD_BOIL("|orange||ul|Passive Effect - Blood Boil",
+      " |yellow|When |red|Bleeding|yellow|, |yellow|all |red|Rage |yellow|gained",
+      " |yellow|is increased by |white|30%"),
+  BLOOD_AND_ICE("|orange||ul|Passive Effect - Blood And Ice",
+      " |yellow|Apply 30% more |cyan|Frost |yellow|to |red|Bleeding |yellow|enemies",
+      " |yellow|Apply 30% more |red|Bleed to |cyan|Frosted |yellow|enemies"),
+  OVERSHIELD("|orange||ul|Passive Effect - Overshield",
+      " |yellow|When your |white|Life |yellow|is full, gain |white|30% |yellow|of",
+      " |yellow|your |white|Life Regen. |yellow|as |white|Barrier Regen."),
+  ELEMENTAL_CRITS_2("|orange||ul|Passive Effect - Status Strike",
+      " |yellow|Critical strikes trigger |teal|elemental status",
+      " |yellow|instead of dealing extra damage"),
+  LETHAL_STRIKE("|orange||ul|Passive Effect - Lethal Strike",
+      " |yellow|Attacks with over 100% Crit. Chance",
+      " |yellow|can trigger a |red||ul|Lethal Strike|yellow|, doubling",
+      " |yellow|crit damage and re-triggering |orange||ul|ON CRIT"),
+  IRON_SCARS("|orange||ul|Passive Effect - Iron Scars",
+      " |yellow|Receive |white|60% |yellow|less bonus damage from",
+      " |yellow|critical strikes"),
+  SOUL_FLAME("|orange||ul|Passive Effect - Blue Flame");
 
   // TODO: We map String to StrifeStat, why not let the user
   //  customize the string rather than declaring it in the enum?
@@ -79,8 +75,8 @@ public enum StrifeTrait {
   private final List<String> additionalText = new ArrayList<>();
 
   StrifeTrait(String trigger, String... additionalText) {
-    this.name = StringExtensionsKt.chatColorize(trigger);
-    this.additionalText.addAll(ListExtensionsKt.chatColorize(Arrays.asList(additionalText)));
+    this.name = PaletteUtil.color(trigger);
+    this.additionalText.addAll(PaletteUtil.color(Arrays.asList(additionalText)));
   }
 
 }
