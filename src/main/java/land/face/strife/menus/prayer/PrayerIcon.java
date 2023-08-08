ab46d4c1fcdce48c9a3c32cd1f8d2c1df22e0c78
Faceguy <kattreinkyle@gmail.com>
2023-08-07 23:18:06 -0400

    Add prayer

---
+++ src/main/java/land/face/strife/menus/prayer/PrayerIcon.java
@@ -0,0 +1,93 @@
+package land.face.strife.menus.prayer;
+
+import com.tealcube.minecraft.bukkit.facecore.utilities.FaceColor;
+import com.tealcube.minecraft.bukkit.facecore.utilities.TextUtils;
+import io.pixeloutlaw.minecraft.spigot.hilt.ItemStackExtensionsKt;
+import java.util.ArrayList;
+import java.util.List;
+import land.face.strife.StrifePlugin;
+import land.face.strife.data.StrifeMob;
+import land.face.strife.data.champion.LifeSkillType;
+import land.face.strife.managers.PrayerManager.Prayer;
+import ninja.amp.ampmenus.events.ItemClickEvent;
+import ninja.amp.ampmenus.items.MenuItem;
+import org.bukkit.Material;
+import org.bukkit.entity.Player;
+import org.bukkit.event.inventory.ClickType;
+import org.bukkit.inventory.ItemStack;
+
+public class PrayerIcon extends MenuItem {
+
+  private final Prayer prayer;
+  private final StrifePlugin plugin;
+
+  PrayerIcon(StrifePlugin plugin, Prayer prayer) {
+    super("", new ItemStack(Material.BARRIER));
+    this.plugin = plugin;
+    this.prayer = prayer;
+  }
+
+  @Override
+  public ItemStack getFinalIcon(Player player) {
+
+    ItemStack stack = new ItemStack(Material.PAPER);
+
+    StrifeMob mob = plugin.getStrifeMobManager().getStatMob(player);
+    if (plugin.getPrayerManager().getPrayerLevelReq().get(prayer) >
+        mob.getChampion().getLifeSkillLevel(LifeSkillType.PRAYER)) {
+      ItemStackExtensionsKt.setCustomModelData(stack, 800);
+      ItemStackExtensionsKt.setDisplayName(stack, FaceColor.WHITE + "Unknown Prayer");
+      TextUtils.setLore(stack, List.of("", FaceColor.GRAY + " ? ? ?", ""), false);
+      return stack;
+    }
+    List<String> lore = new ArrayList<>();
+    if (prayer == Prayer.FIFTEEN || prayer == Prayer.FOURTEEN || prayer == Prayer.THIRTEEN) {
+      // TODO GOD SHIT
+    } else {
+      ItemStackExtensionsKt.setDisplayName(stack, plugin.getPrayerManager().getPrayerNames().get(prayer));
+      lore.addAll(plugin.getPrayerManager().getPrayerLore().get(prayer));
+    }
+    lore.add("");
+    lore.add(FaceColor.WHITE + "Faith: " + (int) mob.getPrayer() + " / " + (int) mob.getMaxPrayer());
+    boolean isActive = plugin.getPrayerManager().isPrayerActive(player, prayer);
+    if (isActive) {
+      ItemStackExtensionsKt.setCustomModelData(stack, plugin.getPrayerManager().getPrayerModelData().get(prayer));
+      lore.add("");
+      lore.add(FaceColor.YELLOW + "Click To Deactivate!");
+    } else if (plugin.getPrayerManager().getPrayerActivationCost().get(prayer) > mob.getPrayer()) {
+      ItemStackExtensionsKt.setCustomModelData(stack, 801);
+      lore.add("");
+      lore.add(FaceColor.RED + "You don't have enough Faith!");
+    } else {
+      ItemStackExtensionsKt.setCustomModelData(stack, plugin.getPrayerManager().getPrayerModelData().get(prayer) + 1);
+      lore.add("");
+      lore.add(FaceColor.GREEN + "Click To Activate!");
+    }
+    TextUtils.setLore(stack, lore, false);
+    return stack;
+  }
+
+  @Override
+  public void onItemClick(ItemClickEvent event) {
+    super.onItemClick(event);
+    if (event.getClickType() == ClickType.DOUBLE_CLICK) {
+      return;
+    }
+    StrifeMob mob = plugin.getStrifeMobManager().getStatMob(event.getPlayer());
+    if (plugin.getPrayerManager().getPrayerLevelReq().get(prayer) >
+        mob.getChampion().getLifeSkillLevel(LifeSkillType.PRAYER)) {
+      return;
+    }
+    if (plugin.getPrayerManager().getPrayerActivationCost().get(prayer) > mob.getPrayer()) {
+      event.setWillUpdate(true);
+      return;
+    }
+    if (plugin.getPrayerManager().isPrayerActive(event.getPlayer(), prayer)) {
+      boolean success = plugin.getPrayerManager().disablePrayer(event.getPlayer(), prayer);
+      event.setWillUpdate(success);
+    } else {
+      boolean success = plugin.getPrayerManager().activatePrayer(event.getPlayer(), prayer);
+      event.setWillUpdate(success);
+    }
+  }
+}
